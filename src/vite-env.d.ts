3488784c13a1f62c4d753f3b4f9237b12669b852
brian <htetlinnoo19@gmail.com>
2026-02-12 21:01:44 +0700

    facebook login button added

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -3,6 +3,8 @@
 interface ImportMetaEnv {
   readonly VITE_AIRTABLE_API_KEY: string;
   readonly VITE_AIRTABLE_BASE_ID: string;
+  readonly VITE_FB_APP_ID: string;
+  readonly VITE_FB_APP_SECRET: string;
 }
 
 interface ImportMeta {
